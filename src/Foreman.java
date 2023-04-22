176defde753616055285df0741c65921ce6ecba2
jakub <jakubgrzegorzewski02@gmail.com>
2023-04-22 18:14:11 +0200

    Implementing Foreman

--- src/Foreman.java
+++ src/Foreman.java
@@ -1,2 +1,29 @@
-public class Foreman {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Foreman extends User{
+    ArrayList<Brigade> brigadeList = new ArrayList<Brigade>();
+    ArrayList<Order> orderList = new ArrayList<Order>();
+
+    Foreman(
+    String name,
+    String surname,
+    Date dateOfBirth,
+    EmployeesDepartment department,
+    String login,
+    String password
+    ) {
+        super(
+            name,
+            surname,
+            dateOfBirth,
+            department,
+            login,
+            password
+        );
+    }
+    public ArrayList<Brigade> getBrigadeList(){
+        System.out.println(this.brigadeList.toString());
+        return this.brigadeList;
+    }
 }
