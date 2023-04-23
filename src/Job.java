fb8a2dacb67cc1ade3f3ddf871123d637b5ab33e
jakub <jakubgrzegorzewski02@gmail.com>
2023-04-23 12:23:32 +0200

    Adding JobID, CounterID, Jobtype's, time, if completed, description,  list of jobs to wait on, list of all jobs, constructor, getObject by ID fo Job.java

--- src/Job.java
+++ src/Job.java
@@ -1,2 +1,46 @@
-public class Job {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Job extends Thread{
+    private int jobID;
+    private int counterID = 0;
+    public enum JobType{
+        GENERAL, ASSEMBLY, DISASSEMBLY, REPLACEMENT;
+    }
+    private JobType jobType;
+    private int time;
+    private boolean completed = false;
+    private String description;
+    private Collection<Job> waitToEnd = new ArrayList<>();
+    public static Map<Integer, Object> allJobs = new HashMap<>();
+
+    Job(JobType jobType, int time, String description){
+        allJobs.put(counterID, this);
+        jobID = counterID++;
+        this.jobType = jobType;
+        this.time = time;
+        this.description = description;
+
+        System.out.println(jobType.toString() + " " + time + " " + description);
+    }
+
+    @Override
+    public void run() {
+        boolean flag = false;
+        while (!flag){
+            flag = true;
+            for (Job job:waitToEnd) {
+                if(!job.completed)
+                    flag = false;
+            }
+        }
+
+        this.completed = true;
+    }
+
+    public static Object getObject(int id){
+        return allJobs.get(id);
+    }
 }
